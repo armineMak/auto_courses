dc1d63780a0045963206baf4b42a5da0f39765fc
armineMak <59016002+armineMak@users.noreply.github.com>
2020-05-28 15:31:21 +0400

    Update pathFile.py

--- test_project/testing_folder/pathFile.py
+++ test_project/testing_folder/pathFile.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 
-driver = webdriver.Chrome(executable_path="C:/Users/Armush/PycharmProjects/test_project/drivers/chromedriver.exe")
+driver = webdriver.Chrome()
 driver.get("http://the-internet.herokuapp.com/login");
 
 username = driver.find_element_by_id("username")
@@ -11,9 +11,6 @@ password = driver.find_element_by_id("password")
 password.send_keys("SuperSecretPassword!")
 
 login = driver.find_element_by_css_selector("#login > button").click()
-
-# success_message = driver.find_element_by_css_selector("#flash")
-
 logout = driver.find_element_by_css_selector("#content > div > a").click()
 
 driver.quit()
